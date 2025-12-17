9f0b336db1331cf9ba5b769a49e390e1dde7476b
sangit-pokhrel <heypsycho339@gmail.com>
2025-12-17 23:05:51 +0545

    Don with About page and Our Services page(Mobile responsiveness left)

---
+++ src/components/Servicespage_component/heroSection.tsx
@@ -0,0 +1,14 @@
+import Section from "@/globals/section";
+const HeroSection = () => {
+  return (
+   <Section>
+     <div className="flex flex-col gap-y-3">
+      <h1 className="font-bold text-4xl text-center">Our Services</h1>
+      <p className="text-center text-md font-extralight">Comprehensive tools and services to accelerate your career growth</p>
+    </div>
+    </Section>
+  )
+}
+
+export default HeroSection;
+      
